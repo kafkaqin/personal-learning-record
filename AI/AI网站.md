ccb904c00fd00c43ebd91d0573f3e8d82852da73
kaka <990570827@qq.com>
2025-04-13 20:39:25 +0800

    工厂方法设计模式

--- AI/AI网站.md
+++ AI/AI网站.md
@@ -5,3 +5,8 @@
 智谱AI
 
 https://d.aigclink.ai/
+
+一些优秀的大语言模型资源
+https://github.com/WangRongsheng/awesome-LLM-resourses
+
+https://github.com/jingyaogong/minimind
--- design_parrern/golang/factory_pattern/factory_pattern.go
+++ design_parrern/golang/factory_pattern/factory_pattern.go
@@ -1,5 +1,14 @@
 package main
 
+/**
+工厂模式是一种创建型设计模式，它的核心思想是将对象的创建过程封装到专门的工厂中，使得客户端无需直接依赖具体的产品类，从而降低了系统的耦合性并提高了扩展性。工厂模式主要包括以下几种变体：
+
+简单工厂模式：由一个工厂类根据参数决定创建哪种产品实例。虽然简单易懂，但违反了“开闭原则”，因为每增加一种产品都需要修改工厂类。
+
+工厂方法模式：通过定义一个抽象工厂接口，由子类决定具体产品的实例化。这样新增产品时，只需增加相应的工厂子类，不需要修改已有代码。
+
+抽象工厂模式：在工厂方法模式的基础上进一步扩展，用于创建一系列相关或相互依赖的产品族。
+*/
 import (
 	"fmt"
 )
@@ -76,4 +85,47 @@ func main() {
 	}
 
 	RenderUI(factory)
+
+	var factory2 ShapeFactory
+	factory2 = &CircleFactory{}
+	circle := factory2.CreateShape()
+	circle.Draw()
+
+	factory2 = &RectangleFactory{}
+	rectangle := factory2.CreateShape()
+	rectangle.Draw()
+}
+
+// -=====new
+type Shape interface {
+	Draw()
+}
+
+type Circle struct{}
+
+func (c *Circle) Draw() {
+	fmt.Println("Drawing a Circle")
+}
+
+type Rectangle struct {
+}
+
+func (r *Rectangle) Draw() {
+	fmt.Println("Drawing a Rectangle")
+}
+
+type ShapeFactory interface {
+	CreateShape() Shape
+}
+
+type CircleFactory struct{}
+
+func (c *CircleFactory) CreateShape() Shape {
+	return &Circle{}
+}
+
+type RectangleFactory struct{}
+
+func (r *RectangleFactory) CreateShape() Shape {
+	return &Rectangle{}
 }
