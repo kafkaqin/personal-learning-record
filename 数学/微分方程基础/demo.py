5c703f211f8bbd547fe1b3add7be92387b667de5
kaka <990570827@qq.com>
2025-05-21 22:57:25 +0800

    组合问题的剪枝操作

--- 数学/微分方程基础/demo.py
+++ 数学/微分方程基础/demo.py
@@ -5,12 +5,17 @@ import matplotlib.pyplot as plt
 def ode_function(t, y):
     return -2*t*y
 
+def system_ode(t, state):
+    x,y = state
+    dxdt = -y
+    dydt=x
 if __name__ == '__main__':
     y0=[1]
     t_span = (0,5)
     t_eval = np.linspace(t_span[0], t_span[1], 100)
     sol = solve_ivp(ode_function, t_span, y0,method="RK45", t_eval=t_eval)
-
+    # initial_state = [1, 0]
+    # sol_system = solve_ivp(fun=system_ode, t_span=(0, 10), y0=initial_state)
 
     plt.plot(sol.t,sol.y[0],label='Numerical Solution')
     plt.plot(sol.t,np.exp(-sol.t**2),'r--',label='Analytical Solution')
@@ -19,3 +24,6 @@ if __name__ == '__main__':
     plt.ylabel('y(t)')
     plt.legend()
     plt.savefig('ODE.png')
+
+    initial_state = [1, 0]
+    sol_system = solve_ivp(fun=system_ode, t_span=(0, 10), y0=initial_state)
