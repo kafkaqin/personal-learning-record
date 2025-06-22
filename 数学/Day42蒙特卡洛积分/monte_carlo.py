78300d23c6360a59c7c730d310332aa6c615eb89
kaka <990570827@qq.com>
2025-06-22 11:50:43 +0800

    数学: Day42蒙特卡洛积分

--- 数学/Day42蒙特卡洛积分/monte_carlo.py
+++ 数学/Day42蒙特卡洛积分/monte_carlo.py
@@ -10,3 +10,7 @@ x_sample = np.random.uniform(a, b, N)
 f_values = integrand(x_sample)
 intervals_estimate = (b-a)*np.mean(f_values)
 print(intervals_estimate)
+
+cumulative_mean = np.cumsum(f_values) /np.arange(1, N+ 1)
+integral_convergence = (b -a) * cumulative_mean
+print(integral_convergence)
