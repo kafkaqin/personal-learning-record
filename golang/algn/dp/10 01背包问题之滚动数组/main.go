b3b29f486344c54c0bc3bcfa2d4d7343b1143730
kaka <990570827@qq.com>
2025-07-05 22:29:23 +0800

    算法: 10 01背包问题之滚动数组

---
+++ golang/algn/dp/10 01背包问题之滚动数组/main.go
@@ -0,0 +1,28 @@
+package main
+
+// 放入物品i 容量为j的背包的最大价值为dp[i][j]
+// dp[i][j] = max(dp[i-1][j],dp[i-1][j-weight]+values[i])
+
+// 一维dp[i]数组
+// dp[j] 容量为j的背包所能装的最大价值为dp[j]
+// 递归公式:max(dp[j],dp[j-weight[i]]+values[i])
+// 初始化: dp[0] = 0
+// 遍历顺序
+// 打印dp数组
+func bag01(n int, bagweight int, weight []int, values []int) int {
+	dp := make([]int, 10)
+	dp[0] = 0
+	for i := 0; i < n; i++ { //物品
+		for j := bagweight; j >= weight[i]; j-- { //背包
+			dp[j] = max(dp[j], dp[j-weight[i]]+values[i])
+		}
+	}
+	return dp[bagweight]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
