1fde070cf731da8d543dd389cdeb88bf82a10f56
kaka <990570827@qq.com>
2025-07-17 22:47:58 +0800

    算法: 22打家劫舍2

--- golang/algn/dp/22打家劫舍2/main.go
+++ golang/algn/dp/22打家劫舍2/main.go
@@ -1,11 +1,15 @@
 package main
 
+// 收尾相连
 // 相邻 不能偷
 // dp数组的含义: 包含下标i 偷到的最大价值为 dp ，结果放在 dp[nums.size-1]
 // 偷i: dp[i-2]+nums[i]
 // 不偷i: dp[i-1]
 func bag(nums []int) int {
 
+	// 1.不考虑 首尾
+	// 2.考虑首元数
+	// 3.考虑尾元数
 	dp := make([]int, len(nums))
 	numsLen := len(nums)
 	dp[0] = nums[0]
@@ -14,7 +18,6 @@ func bag(nums []int) int {
 		dp[i] = 0
 	}
 
-	// 方法2 先遍历背包
 	for i := 1; i < numsLen; i++ { //先遍历背包 排列数 背包就是 s 提供的字符串
 		dp[i] = max(dp[i-1], dp[i-1]+nums[i])
 	}
@@ -27,4 +30,13 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func main() {
+	// 2.考虑首元数
+	// 3.考虑尾元数
+	nums := []int{}
+	a := bag(nums[1:])
+	b := bag(nums[:len(nums)-1])
+	max(a, b)
 }
