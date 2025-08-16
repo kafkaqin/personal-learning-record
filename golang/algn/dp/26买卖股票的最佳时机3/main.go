fc3a78d062865e5aa6a7d63d15900e768c283a3e
kaka <990570827@qq.com>
2025-08-16 23:35:29 +0800

    算法: 26买卖股票的最佳时机3

---
+++ golang/algn/dp/26买卖股票的最佳时机3/main.go
@@ -0,0 +1,38 @@
+package main
+
+// dp[i][0]不操作 dp[i][0] = dp[i-1][0]
+// dp[i][1]第一次持有 dp[i][1] = (dp[i-1][1],dp[i-1][0]-prices[i])
+// dp[i][2]第一次不持有 dp[i][2] = (dp[i-1][2],dp[i-1][1]+prices[i])
+// dp[i][3]第二次持有 dp[i][3] = (dp[i-1][3],dp[i-1][2]-prices[i])
+// dp[i][4]第二次不持有 dp[i][4] = (dp[i-1][4],dp[i-1][3]+prices[i])
+
+func rebTree(prices []int) int {
+	lenPri := len(prices)
+	dp := make([][]int, len(prices))
+	dp[0][0] = 0
+	dp[0][1] = -prices[0]
+	dp[0][2] = 0
+	dp[0][3] = -prices[0]
+	dp[0][4] = 0
+	for i := 1; i < lenPri; i++ {
+		dp[i][0] = 0
+		dp[i][1] = 0
+	}
+
+	for i := 1; i < lenPri; i++ {
+		dp[i][0] = dp[i-1][0]
+		dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+		dp[i][2] = max(dp[i-1][2], dp[i-1][1]+prices[i])
+		dp[i][3] = max(dp[i-1][3], dp[i-1][2]-prices[i])
+		dp[i][4] = max(dp[i-1][4], dp[i-1][3]+prices[i])
+	}
+
+	return dp[lenPri-1][4]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
