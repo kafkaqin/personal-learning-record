02b3961b503028d202d48aa20d7deb8752500ec3
kaka <990570827@qq.com>
2025-08-23 12:18:16 +0800

    算法: 33最长公共子系列

---
+++ golang/algn/dp/33最长公共子系列/main.go
@@ -0,0 +1,34 @@
+package main
+
+// dp[i][j] 以i-1为结尾的str1[i]，以j-1为结尾的str2[j]最长公共子系列的长度
+// if str1[i-1] > str2[j-1] {dp[i][j] = dp[i-1][j-1]+1}
+func getMaxLength(str1, str2 []string) int {
+	lenstr1 := len(str1)
+	lenstr2 := len(str2)
+	dp := make([][]int, lenstr1+1)
+	dp[0][0] = 0
+	for i := 1; i <= lenstr1; i++ {
+		dp[i][0] = 0
+	}
+	for i := 1; i <= lenstr1; i++ {
+		dp[0][i] = 0
+	}
+	for i := 1; i <= lenstr1; i++ {
+		for j := 1; j <= lenstr2; j++ {
+			if str1[i-1] > str2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+
+		}
+	}
+	return dp[lenstr1][lenstr2]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
