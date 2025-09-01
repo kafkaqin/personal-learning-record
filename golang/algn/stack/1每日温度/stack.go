efa14d089bc2d285aac6aecd1358fab95d4340d8
kaka <990570827@qq.com>
2025-09-01 22:44:03 +0800

    算法: 1每日温度

---
+++ golang/algn/stack/1每日温度/stack.go
@@ -0,0 +1,18 @@
+package _每日温度
+
+func stacks(T []int) []int {
+	stack := make([]int, len(T))
+	result := make([]int, len(T))
+	stack = append(stack, 0)
+	for i := 1; i < len(T); i++ {
+		if T[i] <= T[stack[len(stack)-1]] { //小于等于栈顶元素
+			stack = append(stack, i) //进栈
+		} else { //T[i] > T[stack[len(stack)-1]
+			for len(stack) > 0 && T[i] > T[stack[len(stack)-1]] {
+				result[i] = i - stack[len(stack)-1]
+				stack = stack[:len(stack)-1] //出栈
+			}
+		}
+	}
+	return result
+}
